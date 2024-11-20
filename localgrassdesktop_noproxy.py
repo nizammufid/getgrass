900df6453fd8c3e491c64184cf3b062214b061c3
ylasgamers <ylasgamers@gmail.com>
2024-11-21 01:34:54 +0700

    Update localgrassdesktop_noproxy.py

--- localgrassdesktop_noproxy.py
+++ localgrassdesktop_noproxy.py
@@ -52,7 +52,7 @@ async def connect_to_wss(user_id):
                                 "user_agent": custom_headers['User-Agent'],
                                 "timestamp": int(time.time()),
                                 "device_type": "desktop",
-                                "version": "4.28.2",
+                                "version": "4.29.0",
                             }
                         }
                         logger.debug(auth_response)
